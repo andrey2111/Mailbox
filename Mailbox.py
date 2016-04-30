ac69d22ce7064c555e0e078a98d1e85f1b17ae41
Andrey <andrey2111@mail.ru>
2016-04-30 22:43:28 +0300

    startswith

--- Mailbox.py
+++ Mailbox.py
@@ -5,9 +5,9 @@ inf = open(inputfilename, 'r')
 openedfiles[inputfilename] = inf #открытие входного файла на чтение и добавление его в словарь
 
 for line in inf:
-    if len(line.split()) and line.split()[0] == 'From': #Если строка начинается с From
+    if line.startswith("From "): #Если строка начинается с From
         email = line.split()[1] #email отправителя
-        if email not in openedfiles:#если файл с именем email не открыт, его необходимо открыть
+        if email not in openedfiles: #если файл с именем email не открыт, его необходимо открыть
             ouf = open(email, 'w')
             openedfiles[email] = ouf #добавляем открытый файл в словарь
 
